77355c3a184dfa797ea5ef274daadf8d662c1634
Yassine Bounekhla <56373201+rudream@users.noreply.github.com>
2023-05-19 15:58:54 +0100

    add polyfill for randomuuid (#26368)

--- web/.storybook/preview.js
+++ web/.storybook/preview.js
@@ -21,6 +21,8 @@ import theme from './../packages/design/src/theme';
 import DefaultThemeProvider from './../packages/design/src/ThemeProvider';
 import Box from './../packages/design/src/Box';
 import TeletermThemeProvider from './../packages/teleterm/src/ui/ThemeProvider';
+import '../packages/teleport/src/lib/polyfillRandomUuid';
+import { ThemeProvider as TeletermThemeProvider } from './../packages/teleterm/src/ui/ThemeProvider';
 import { handlersTeleport } from './../packages/teleport/src/mocks/handlers';
 
 // Checks we are running non-node environment (browser)
